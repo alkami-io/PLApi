634f08d32fc91a73fd48aa984e48766d4d08c831
AlkamiDev <robert@alkami.io>
2018-12-16 14:09:47 -0700

    Update: Refactor of ApiFootball Connection Service (#7)
    Refactored ApiFootball Connection service to handle only connecting to API
    Added Services for Fixtures, Leagues, Seasons and Teams as part of API Footall Module.

---
+++ app/services/api_football_api/api_football_season_service.rb
@@ -0,0 +1,26 @@
+require 'api_football_api/api_football_connection_service'
+
+module ApiFootball
+  module SeasonService
+    class Seasons
+      attr_reader :api_connection
+
+      def initialize
+        @api_connection = ApiFootball::ConnectionService.api_connection
+      end
+
+      def all_seasons
+        response = api_connection.get('seasons')
+        JSON.parse(response.body)
+      end
+
+      def current_season
+        # hold if needed
+      end
+
+      def last_season
+        # hold if needed
+      end
+    end
+  end
+end
