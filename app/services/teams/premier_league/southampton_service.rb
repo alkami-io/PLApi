60bb5cd9f31ae74f021a4b1ce3c82ae1285495fe
alkami-io <robert@alkami.io>
2018-12-16 12:21:31 -0700

    Update: Corrected spelling on Southampton, removing extra h

--- app/services/teams/premier_league/southampton_service.rb
+++ app/services/teams/premier_league/southampton_service.rb
@@ -1,6 +1,6 @@
 module Teams
   module PremierLeague
-    class SouthhamptonService
+    class SouthamptonService
       def self.team_details
         conn = ApiFootballApiService.new
         response = conn.connection.get('teams/team/41')
