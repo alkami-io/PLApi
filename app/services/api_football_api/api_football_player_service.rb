05303ba99b8f6563a58901879e7c8f24fffdc663
alkami-io <robert@alkami.io>
2018-12-16 15:20:34 -0700

    Update: Added Player Service and Standings Service Added player service to api_football service collection Added Standings service to api_football service collection

---
+++ app/services/api_football_api/api_football_player_service.rb
@@ -0,0 +1,18 @@
+require 'api_football_api/api_football_connection_service'
+
+module ApiFootball
+  module PlayerService
+    class Players
+      attr_reader :api_connection
+
+      def initialize
+        @api_connection = ApiFootball::ConnectionService.api_connection
+      end
+
+      def players_by_season_team(season, team_id)
+        response = api_connection.get("players/#{season}/#{team_id}")
+        JSON.parse(response.body)
+      end
+    end
+  end
+end
