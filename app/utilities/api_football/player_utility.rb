280d1c416a4924b4d1db451759cfaf069e70635f
alkami-io <robert@alkami.io>
2018-12-29 22:16:00 -0700

    Update: Refactor of players serice Refactored player service into a utility added write to json method and manually tested.

--- app/utilities/api_football/player_utility.rb
+++ app/utilities/api_football/player_utility.rb
@@ -1,23 +1,24 @@
 require "#{Rails.root}/app/utilities/api_football/connection_utility.rb"
 
 module ApiFootball
-  module LineupUtility
-    class Lineups
+  module PlayerUtility
+    class Players
       attr_reader :api_connection
 
       def initialize
         @api_connection = ApiFootball::ConnectionUtility::ApiConnection.new
       end
 
-      def by_fixture(write, fixture_id)
-        response = api_connection.connection.get("lineups/#{fixture_id}")
-        filename = "lineups_by_fixture_id_#{fixture_id}_#{DateTime.current}"
+      # Endpoint: /players/{season}/{team_id}
+      def by_season_team(write, season, team_id)
+        response = api_connection.connection.get("players/#{season}/#{team_id}")
+        filename = "players_by_season_#{season}_team_#{team_id}_#{DateTime.current}"
 
         write == true ? write_to_json(filename, response.body) : JSON.parse(response.body)
       end
 
       def write_to_json(filename, response)
-        File.open("#{Rails.root}/raw_data/json_files/premier_league_data/api_football/lineups/#{filename}.json","w") do |f|
+        File.open("#{Rails.root}/raw_data/json_files/premier_league_data/api_football/players/#{filename}.json","w") do |f|
           f.write(response)
         end
       end
