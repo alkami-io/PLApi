0f640423e48c3e23f68bf8262e671e27bae9968e
alkami-io <robert@alkami.io>
2018-12-29 21:17:42 -0700

    Update: Refactor League Utility Refactored League utility wired up write to json method and manually tested.

---
+++ app/utilities/api_football/league_utility.rb
@@ -0,0 +1,47 @@
+require "#{Rails.root}/app/utilities/api_football/connection_utility.rb"
+
+module ApiFootball
+  module LeagueUtility
+    class Leagues
+      attr_reader :api_connection
+
+      def initialize
+        @api_connection = ApiFootball::ConnectionUtility::ApiConnection.new
+      end
+
+      def all_leagues(write)
+        response = api_connection.connection.get('leagues')
+        filename = "leagues_#{DateTime.current}"
+
+        write == true ? write_to_json(filename, response.body) : JSON.parse(response.body)
+      end
+
+      def by_id(write, league_id)
+        response = api_connection.connection.get("leagues/league/#{league_id}")
+        filename = "leagues_by_id_#{league_id}_#{DateTime.current}"
+
+        write == true ? write_to_json(filename, response.body) : JSON.parse(response.body)
+      end
+
+      def by_season(write, season)
+        response = api_connection.connection.get("leagues/season/#{season}")
+        filename = "leagues_by_season_#{season}_#{DateTime.current}"
+
+        write == true ? write_to_json(filename, response.body) : JSON.parse(response.body)
+      end
+
+      def by_country_season(write, country_name, season)
+        response = api_connection.connection.get("leagues/country/#{country_name}/#{season}")
+        filename = "leagues_by_country_season_#{country_name}_#{season}_#{DateTime.current}"
+
+        write == true ? write_to_json(filename, response.body) : JSON.parse(response.body)
+      end
+
+      def write_to_json(filename, response)
+        File.open("#{Rails.root}/raw_data/json_files/premier_league_data/api_football/leagues/#{filename}.json","w") do |f|
+          f.write(response)
+        end
+      end
+    end
+  end
+end
