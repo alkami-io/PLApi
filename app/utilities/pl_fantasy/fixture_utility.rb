9da6e960b8c8420a031784ab54fec9f7f6650104
alkami-io <robert@alkami.io>
2018-12-30 22:20:51 -0700

    Update: Added Fixture Utility Added fixture utility with the fixtures and fixtures by event week actions wired to core utilities manually tested against api to verify functionality

---
+++ app/utilities/pl_fantasy/fixture_utility.rb
@@ -0,0 +1,42 @@
+require "#{Rails.root}/app/utilities/pl_fantasy/connection_utility.rb"
+require "#{Rails.root}/app/utilities/core_utilities/data_to_json.rb"
+
+module PLFantasy
+  module FixtureUtility
+    class Fixtures
+      include CoreUtility::DataToJSON
+
+      attr_reader :api_connection
+
+      def initialize
+        @api_connection = PLFantasy::ConnectionUtility::ApiConnection.new
+      end
+
+      # Endpoint: /fixtures
+      # wod: WriteOrDisplay Pass "w" to Write to File or "d" to Display data
+      def fixtures(wod)
+        options = {
+          wod: wod,
+          response: api_connection.connection.get("fixtures/"),
+          directory: "pl_fantasy_data/pulled_data/fixtures",
+          filename: "fixtures_#{DateTime.current.strftime("%C%y-%m-%d")}"
+        }
+
+        CoreUtility::DataToJSON.write_or_display_data(options)
+      end
+
+      # Endpoint: /fixtures?event={event_id}
+      # wod: WriteOrDisplay Pass "w" to Write to File or "d" to Display data
+      def fixtures_by_event(wod, event_id)
+        options = {
+          wod: wod,
+          response: api_connection.connection.get("fixtures/?event=#{event_id}"),
+          directory: "pl_fantasy_data/pulled_data/fixtures_by_event",
+          filename: "fixtures_by_event_#{event_id}_#{DateTime.current.strftime("%C%y-%m-%d")}"
+        }
+
+        CoreUtility::DataToJSON.write_or_display_data(options)
+      end
+    end
+  end
+end
