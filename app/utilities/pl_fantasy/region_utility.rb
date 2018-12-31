48ac38e56f4c848369b9c7c982ad7be30149a494
alkami-io <robert@alkami.io>
2018-12-30 22:29:28 -0700

    Update: Added Region Utility Added region utility Wired core utilities manually tested against api to verify functionality

---
+++ app/utilities/pl_fantasy/region_utility.rb
@@ -0,0 +1,29 @@
+require "#{Rails.root}/app/utilities/pl_fantasy/connection_utility.rb"
+require "#{Rails.root}/app/utilities/core_utilities/data_to_json.rb"
+
+module PLFantasy
+  module RegionUtility
+    class Regions
+      include CoreUtility::DataToJSON
+
+      attr_reader :api_connection
+
+      def initialize
+        @api_connection = PLFantasy::ConnectionUtility::ApiConnection.new
+      end
+
+      # Endpoint: /regions
+      # wod: WriteOrDisplay Pass "w" to Write to File or "d" to Display data
+      def regions(wod)
+        options = {
+          wod: wod,
+          response: api_connection.connection.get("region/"),
+          directory: "pl_fantasy_data/pulled_data/regions",
+          filename: "regions_#{DateTime.current.strftime("%C%y-%m-%d")}"
+        }
+
+        CoreUtility::DataToJSON.write_or_display_data(options)
+      end
+    end
+  end
+end
